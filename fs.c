6e99a8b6bbc60657160e3a5d62ef38a134d7a941
Danny Cho <dannycho7@gmail.com>
2019-03-17 16:53:20 -0700

    make_fs and umount_fs

--- fs.c
+++ fs.c
@@ -1,2 +1,100 @@
+#include <math.h>
+#include <string.h>
 #include "disk.h"
 #include "fs.h"
+#define DATA_BLOCKS 4096
+#define FILE_MAX 64
+#define FILE_NAME_MAX 15
+#define get_nblocks(obj) (int) ceil((double)(sizeof(obj))/BLOCK_SIZE)
+
+static struct SuperBlock {
+	int fat_start;
+	int root_dir_start;
+	int data_block_start;
+} sblock;
+/*
+ * fat[i] = {
+ * -1 if data block i unallocated, 
+ * x in [0, DATA_BLOCKS) if data block i allocated and is not eof			 
+ * DATA_BLOCKS if data block i allocated and is eof
+ * }
+ */
+static int fat[DATA_BLOCKS];
+
+struct FileMetadata {
+	char name[FILE_NAME_MAX+1];
+	int data_block_i; // index of first data block
+	int size;
+	int valid; // 0 if invalid and 1 if valid
+};
+
+struct Directory {
+	struct FileMetadata files[FILE_MAX];
+};
+
+static struct Directory root_dir;
+
+static int batch_block_write(int block_start, char* buf, size_t size, int (*get_next_block)(int)) {
+	int block = block_start;
+	int nbytes_written = 0;
+	while (nbytes_written != size) {
+		if (block_write(block, buf) == -1)
+			break;
+		int needed = (size - nbytes_written > BLOCK_SIZE) ? BLOCK_SIZE : size - nbytes_written;
+		buf += needed;
+		nbytes_written += needed;
+		block = get_next_block(block);
+	}
+	return nbytes_written;
+}
+
+static int incr(int x) { return x + 1; }
+static int write_metadata(int block_start, char* buf, size_t size) {
+	return batch_block_write(block_start, buf, size, incr);
+}
+
+int make_fs(char* disk_name) {
+	if (make_disk(disk_name) == -1)
+		return -1;
+	struct SuperBlock fs_sblock;
+	fs_sblock.fat_start = get_nblocks(fs_sblock);
+	int fs_fat[DATA_BLOCKS];
+	for (int i = 0; i < DATA_BLOCKS; i++) {
+		fs_fat[i] = -1;
+	}
+	fs_sblock.root_dir_start = fs_sblock.fat_start + get_nblocks(fs_fat);
+	struct Directory fs_root_dir;
+	for (int i = 0; i < FILE_MAX; i++) {
+		fs_root_dir.files[i].valid = 0;
+	}
+	fs_sblock.data_block_start = fs_sblock.root_dir_start + get_nblocks(fs_root_dir);
+
+	if (open_disk(disk_name) == -1 ||
+		write_metadata(0, (char*) (&fs_sblock), sizeof(fs_sblock)) != sizeof(fs_sblock) ||
+		write_metadata(fs_sblock.fat_start, (char*) (&fs_fat), sizeof(fs_fat)) != sizeof(fs_fat) ||
+		write_metadata(fs_sblock.root_dir_start, (char*) (&fs_root_dir), sizeof(fs_root_dir)) != sizeof(fs_root_dir) ||
+		close_disk() == -1)
+		return -1;
+	return 0;
+}
+
+int mount_fs(char* disk_name);
+int umount_fs(char* disk_name) {
+	if (write_metadata(0, (char*) (&sblock), sizeof(sblock)) != sizeof(sblock) ||
+		write_metadata(sblock.fat_start, (char*) (&fat), sizeof(fat)) != sizeof(fat) ||
+		write_metadata(sblock.root_dir_start, (char*) (&root_dir), sizeof(root_dir)) != sizeof(root_dir) ||
+		close_disk() == -1)
+		return -1;
+
+	return 0;
+}
+
+int fs_open(char* name);
+int fs_close(int fildes);
+int fs_create(char* name);
+int fs_delete(char* name);
+int fs_read(int fildes, void* buf, size_t nbyte);
+int fs_write(int fildes, void* buf, size_t nbyte);
+int fs_get_filesize(int fildes);
+int fs_lseek(int fildes, off_t offset);
+int fs_truncate(int fildes, off_t length);
